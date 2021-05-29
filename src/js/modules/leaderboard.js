ab85faf39f370dd1e3c700eadd2899674c09e395
Admin <numelen@gmail.com>
2021-05-29 16:28:23 +0300

    fix: fixed bug with empty records

--- src/js/modules/leaderboard.js
+++ src/js/modules/leaderboard.js
@@ -10,31 +10,34 @@ export default class Leaderboard {
   buildTable() {
     this.container.innerHTML = '';
     const data = JSON.parse(localStorage.getItem(`records${this.fieldSize.value}`));
-    let rows = '';
-    data.forEach((item) => {
-      rows += `
-      <tbody class="records__tbody">
-        <tr class="records__row">
-          <th class="records__cell">${item.moves}</th>
-          <th class="records__cell">${item.time}</th>
-          <th class="records__cell">${item.date}</th>
-        </tr>
-      </tbody>`;
-    });
-    const table = `
-      <table class="records">
-      <caption class="records__caption">Best scores ${this.fieldSize.value === '9' ? '3x3' : '4x4'}</caption>
-      <thead class="records__thead">
-        <tr class="records__row">
-          <th class="records__cell">Moves</th>
-          <th class="records__cell">Time</th>
-          <th class="records__cell">Date</th>
-        </tr>
-      </thead>
-      ${rows}
-    </table>`;
-
-    this.container.innerHTML += table;
+    if (data) {
+      let rows = '';
+      data.forEach((item) => {
+        rows += `
+        <tbody class="records__tbody">
+          <tr class="records__row">
+            <th class="records__cell">${item.moves}</th>
+            <th class="records__cell">${item.time}</th>
+            <th class="records__cell">${item.date}</th>
+          </tr>
+        </tbody>`;
+      });
+      const table = `
+        <table class="records">
+        <caption class="records__caption">Best scores ${this.fieldSize.value === '9' ? '3x3' : '4x4'}</caption>
+        <thead class="records__thead">
+          <tr class="records__row">
+            <th class="records__cell">Moves</th>
+            <th class="records__cell">Time</th>
+            <th class="records__cell">Date</th>
+          </tr>
+        </thead>
+        ${rows}
+      </table>`;
+      this.container.innerHTML += table;
+    } else {
+      this.container.innerHTML += '<div class="records__notice">No data yet (:</div>';
+    }
     this.container.innerHTML += '<a href="#" class="popup-records__close">Back</a>';
     this.container.style.display = 'flex';
     document.querySelector('.popup-records__close').addEventListener('click', (event) => {
